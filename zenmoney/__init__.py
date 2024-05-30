f54149b763772898c09b9c5a5bbdc57908bae7b5
Nikita Zvekov <cooper30@mail.ru>
2024-05-30 17:54:41 +0400

    Init

--- zenmoney/__init__.py
+++ zenmoney/__init__.py
@@ -1,3 +1,4 @@
-from .models import Diff  # noqa:F401
+from models.diff import Diff  # noqa:F401
+
 from .oauth2 import ZenmoneyOAuth2  # noqa:F401
 from .request import ZenmoneyRequest  # noqa:F401
