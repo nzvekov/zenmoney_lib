9a8c0cfd9452e2366355622304c77d5185642b82
Nikita Zvekov <cooper30@mail.ru>
2024-06-12 17:38:06 +0400

    fix setup.py

--- zenmoney/__init__.py
+++ zenmoney/__init__.py
@@ -1,6 +1,3 @@
-import models
-
-from .oauth2 import ZenmoneyOAuth2
-from .request import ZenmoneyRequest
-
-__all__ = ("ZenmoneyOAuth2", "ZenmoneyRequest", "models")
+from . import models as zenmodels  # noqa
+from .oauth2 import ZenmoneyOAuth2  # noqa
+from .request import ZenmoneyRequest  # noqa
