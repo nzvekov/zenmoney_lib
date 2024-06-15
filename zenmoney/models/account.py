265705e51276b710b1287e39129c59ae7d2c71c0
Nikita Zvekov <cooper30@mail.ru>
2024-06-15 14:03:26 +0400

    fix user model

--- zenmoney/models/account.py
+++ zenmoney/models/account.py
@@ -28,7 +28,6 @@ class Account:
     archive: bool
     balance: float
     private: bool
-    savings: bool
     enable_sms: bool
     in_balance: bool
     instrument: int
@@ -37,6 +36,7 @@ class Account:
     enable_correction: bool
     balance_correction_type: BalanceCorrectionType
     changed: int
+    savings: Optional[bool] = None
     role: Optional[int] = None
     sync_id: Optional[List[str]] = None
     company: Optional[int] = None
@@ -61,7 +61,6 @@ class Account:
             balance=from_float(obj.get("balance")),
             changed=from_int(obj.get("changed")),
             private=from_bool(obj.get("private")),
-            savings=from_bool(obj.get("savings")),
             enable_sms=from_bool(obj.get("enableSMS")),
             in_balance=from_bool(obj.get("inBalance")),
             instrument=from_int(obj.get("instrument")),
@@ -69,6 +68,7 @@ class Account:
             start_balance=from_float(obj.get("startBalance")),
             enable_correction=from_bool(obj.get("enableCorrection")),
             balance_correction_type=BalanceCorrectionType(obj.get("balanceCorrectionType")),
+            savings=from_union([from_none, from_bool], obj.get("savings")),
             role=from_union([from_none, from_int], obj.get("role")),
             sync_id=from_union([from_none, lambda x: from_list(from_str, x)], obj.get("syncID")),
             company=from_union([from_none, from_int], obj.get("company")),
@@ -91,7 +91,6 @@ class Account:
             "balance": to_float(self.balance),
             "changed": from_int(self.changed),
             "private": from_bool(self.private),
-            "savings": from_bool(self.savings),
             "enableSMS": from_bool(self.enable_sms),
             "inBalance": from_bool(self.in_balance),
             "instrument": from_int(self.instrument),
@@ -99,6 +98,7 @@ class Account:
             "startBalance": to_float(self.start_balance),
             "enableCorrection": from_bool(self.enable_correction),
             "balanceCorrectionType": to_enum(BalanceCorrectionType, self.balance_correction_type),
+            "savings": from_union([from_none, from_bool], self.savings),
             "role": from_union([from_none, from_int], self.role),
             "syncID": from_union([from_none, lambda x: from_list(from_str, x)], self.sync_id),
             "company": from_union([from_none, from_int], self.company),
